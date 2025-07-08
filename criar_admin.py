38b38cba2bce2f31e1d0b7b5aa006ec46b2acf84
Coopex-entregas <coopexentregas.rn@gmail.com>
2025-07-08 20:11:37 -0300

    rfgt

--- criar_admin.py
+++ criar_admin.py
@@ -1,11 +1,15 @@
-from app import db, Usuario
+from app import app, db, Usuario
 from werkzeug.security import generate_password_hash
 
-with db.app.app_context():
+with app.app_context():
     if not Usuario.query.filter_by(nome="coopex").first():
-        admin = Usuario(nome="coopex", senha_hash=generate_password_hash("05062721"), tipo="adm")
+        admin = Usuario(
+            nome="coopex",
+            senha_hash=generate_password_hash("05062721"),
+            tipo="adm"
+        )
         db.session.add(admin)
         db.session.commit()
-        print("Admin criado com sucesso!")
+        print("✅ Admin criado com sucesso!")
     else:
-        print("Admin já existe.")
+        print("⚠️ Admin já existe.")
